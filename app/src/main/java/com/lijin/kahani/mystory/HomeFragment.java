b4930a891bc39b4f40ed7858a5aa25b2a1006ad7
lijinc <lijinc100@gmail.com>
2015-01-04 12:49:54 +0530

    Implimented autoscroll update recylerView

--- app/src/main/java/com/lijin/kahani/mystory/HomeFragment.java
+++ app/src/main/java/com/lijin/kahani/mystory/HomeFragment.java
@@ -1,4 +1,5 @@
 package com.lijin.kahani.mystory;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.lijin.kahani.backend.bookApi.model.Book;
+import com.lijin.kahani.backend.bookApi.model.CollectionResponseBook;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +21,15 @@ import java.util.List;
 public class HomeFragment extends Fragment {
     CardAdapter cardAdapter=null;
     List<Book> bookList;
+    String nextPageToken=null;
+    Boolean endOfList=false;
+    private boolean loading = true;
+    Context mActivity;
+    HomeFragment homeFragment;
+    int pastVisiblesItems;
+    int visibleItemCount;
+    int totalItemCount;
+
     // newInstance constructor for creating fragment with arguments
     public static HomeFragment newInstance() {
         HomeFragment fragmentHome = new HomeFragment();
@@ -33,19 +45,58 @@ public class HomeFragment extends Fragment {
     // Inflate the view for the fragment based on layout XML
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mActivity=getActivity();
+        homeFragment=this;
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        RecyclerView recList = (RecyclerView) view.findViewById(R.id.card_recycler_view);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        final RecyclerView recList = (RecyclerView) view.findViewById(R.id.card_recycler_view);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         bookList=new ArrayList<Book>();
         cardAdapter=new CardAdapter(bookList,getActivity());
         recList.setLayoutManager(linearLayoutManager);
         recList.setAdapter(cardAdapter);
-        new StoryListEndpointsAsyncTask(getActivity(),this,"HOME").execute();
+        //Todo update only on scrolldown
+        recList.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                visibleItemCount = linearLayoutManager.getChildCount();
+                totalItemCount = linearLayoutManager.getItemCount();
+                pastVisiblesItems = linearLayoutManager.findFirstVisibleItemPosition();
+                    if (loading) {
+                        if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
+
+                            loading = false;
+                            new BookListEndpointsAsyncTask(mActivity, homeFragment, "HOME").execute(nextPageToken);
+                        }
+                    }
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                loading=true;
+            }
+        });
+
+        new BookListEndpointsAsyncTask(getActivity(),this,"HOME").execute();
         return view;
     }
 
-    public void onHomeListLoad(List<Book> bookList) {
-        cardAdapter.setList(bookList);
+    public void onHomeListLoad(CollectionResponseBook bookListPair) {
+        if(bookListPair!=null) {
+            if(bookListPair.getItems()!=null){
+                for (Book book:bookListPair.getItems()){
+                    bookList.add(book);
+                }
+                cardAdapter.notifyDataSetChanged();
+            }
+            if(bookListPair.getNextPageToken()==nextPageToken){
+                endOfList=true;
+            }
+            else {
+                nextPageToken=bookListPair.getNextPageToken();
+            }
+        }
+
     }
 }
